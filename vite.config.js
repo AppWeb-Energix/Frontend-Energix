bd3e3cf27b762fbf4b58325bcf720962d2b0c1d2
Bryan Eduardo Barba Estrada <u202323479@upc.edu.pe>
2025-10-08 18:37:34 -0500

    feat: configure GitHub Pages deployment with base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
     plugins: [vue()],
+    base: '/Frontend-Energix/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
