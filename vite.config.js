264e8adb39ec67769d4c2c34dbe168c1715540d2
Alexiz248 <alexisfyd@gmail.com>
2025-10-08 18:39:08 -0500

    Update vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+    base: '/Frontend-Energix/',
     plugins: [vue()],
     base: '/Frontend-Energix/',
     resolve: {
