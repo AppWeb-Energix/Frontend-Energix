dce2119456ddcf3cebdcd067c16b41201016fc56
IamAndreek <u20231b120@upc.edu.pe>
2025-12-02 17:40:38 -0500

    feat(hotfix-profile): fix settings

---
+++ src/personalization/domain/profile.entity.js
@@ -0,0 +1,55 @@
+/**
+}
+    }
+        };
+            updatedAt: this.updatedAt
+            createdAt: this.createdAt,
+            district: this.district,
+            dni: this.dni,
+            email: this.email,
+            lastName: this.lastName,
+            firstName: this.firstName,
+            username: this.username,
+            userId: this.userId,
+        return {
+    toJson() {
+
+    }
+        });
+            updatedAt: json.updatedAt
+            createdAt: json.createdAt,
+            district: json.district,
+            dni: json.dni,
+            email: json.email,
+            lastName: json.lastName,
+            firstName: json.firstName,
+            username: json.username,
+            userId: json.userId,
+        return new UserProfile({
+    static fromJson(json) {
+
+    }
+        this.updatedAt = updatedAt;
+        this.createdAt = createdAt;
+        this.district = district;
+        this.dni = dni;
+        this.email = email;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.username = username;
+        this.userId = userId;
+    }) {
+        updatedAt
+        createdAt,
+        district,
+        dni,
+        email,
+        lastName,
+        firstName,
+        username,
+        userId,
+    constructor({
+export class UserProfile {
+ */
+ * Profile Entity - Representa la información del perfil de usuario
+
